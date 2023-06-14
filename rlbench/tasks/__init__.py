cb719cdff9dba92ecf039619c2f8be6b0d12057f
rgarciap <rjgpinel@gmail.com>
2023-06-15 00:40:49 +0200

    tasks/tower3: add

--- rlbench/tasks/__init__.py
+++ rlbench/tasks/__init__.py
@@ -110,6 +110,7 @@ from rlbench.tasks.unplug_charger import UnplugCharger
 from rlbench.tasks.water_plants import WaterPlants
 from rlbench.tasks.weighing_scales import WeighingScales
 from rlbench.tasks.wipe_desk import WipeDesk
+from rlbench.tasks.tower3 import Tower3
 
 FS10_V1 = {
     'train': [
